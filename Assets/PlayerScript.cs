4291f64a76c83f0f359633531b82e4120a65215e
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-07-20 23:14:13 +0200

    adding collision to walls and ground, works

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb=GetComponent<Rigidbody>();
+        rb.velocity=Vector3.zero;
     }
 
     // Update is called once per frame
