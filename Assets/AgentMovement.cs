0634fa22e18305b8bf82bac6d966a642f649dd92
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-07-21 16:57:15 +0200

    redoing after crash and adding AI code work ?

--- Assets/AgentMovement.cs
+++ Assets/AgentMovement.cs
@@ -1,39 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class AgentMovement : MonoBehaviour
 {
     public UnityEngine.AI.NavMeshAgent agent;
-    public Transform player;
+    public GameObject player;
      AudioSource m_audio;
-    private bool playRoar;
-    private Animator animator;
+   
+    Animator animator;
+    const string STAND_STATE="Stand";
+    const string WALK_STATE="Walk";
+    const string ATTACK_STATE="Attack";
 
-    /*
-        public float health;*/
-
-        public LayerMask whatIsGround, whatIsPlayer;
-
-        //Patrolling
-        public Vector3 walkPoint;
-        bool walkPointSet;
-        public float walkPointRange;
-
-        //Attacking
-        public float timeBetweenAttack;
-        bool alreadyAttacked;
-        public GameObject projectile; //A supprimer ou modifier
-
-        //States
-      /*  public float sightRange, attackRange;
-        public bool playerInSightRange, playerInAttackRange;*/
+//Actual Action
+    public string currentAction;
+ 
     private void Awake()
     {
-        /*player=GameObject.Find("Dwarf BerserkerM").transform; //Voir s'il faut pas changer le nom
-        agent=GetComponent<UnityEngine.AI.NavMeshAgent>();*/
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        animator = GetComponent<Animator>();
+       currentAction = STAND_STATE;
+       
     }
 
     public void Shout()
@@ -45,97 +31,84 @@ public class AgentMovement : MonoBehaviour
     void Start()
     {
         m_audio=GetComponent<AudioSource>();
-        Patroling();
+         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        Walk();
     }
-public void PrintEvent(string s)
-    {
-        Debug.Log("PrintEvent: " + s + " called at: " + Time.time);
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        /* playerInSightRange=Physics.CheckSphere(transform.position,sightRange,whatIsPlayer);
-         playerInAttackRange=Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
-         if (!playerInSightRange && !playerInAttackRange)*/
-          Patroling();
-        /* if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-         if (playerInAttackRange && playerInSightRange) AttackPlayer();*/
-        /* Vector2 input =new Vector2(Input.GetAxis("Horizontal"), Input.getAxis("Vertical"));*/
+        
         if (Input.GetKeyDown(KeyCode.M))
         {
          //   animator.SetBool("Walk", true);
             Debug.Log(animator.GetBool("Walk"));
             
         }
-      /*  if (Input.GetKeyDown(KeyCode.L))
-        {
-            animator.SetBool("Walk", false);
-            Debug.Log(animator.GetBool("Walk"));
-        }*/
+     
         if (Input.GetKeyDown(KeyCode.G))
         {
             animator.SetTrigger("ToShout");
 
             Debug.Log("Must Shout");
            
-           //m_audio.Play();
-           // Debug.Log(animator.GetBool("Walk"));
+        }
+        if(player !=null)
+        {
+            if(MovingToTarget())
+            {
+                return;
+            }
+            else{
+                Attack();
+            }
         }
     }
+
+    bool MovingToTarget()
+    {
+        agent.SetDestination(player.transform.position);
+        if(agent.remainingDistance==0)
+            return false;
+        if(agent.remainingDistance>agent.stoppingDistance)
+        {
+            if(currentAction !=WALK_STATE)
+                Walk();
+        }
+        else
+        {
+            RotateToTarget(player.transform);
+            return false;
+        }
+        return true;
+    }
+
+    void Walk()
+    {
+        ResetAnimation();
+        currentAction=WALK_STATE;
+        animator.SetBool(WALK_STATE,true);
+    }
+      
+    void Attack()
+    {
+        ResetAnimation();
+        currentAction=ATTACK_STATE;
+        animator.SetBool(ATTACK_STATE,true);
+    }
+
+    private void ResetAnimation()
+    {
+        animator.SetBool(WALK_STATE,false);
+        animator.SetBool(ATTACK_STATE,false);
+    }
     
-        private void Patroling()
-        {
-            if (!walkPointSet) SearchWalkPoint();
-            Debug.Log("Start Partol");
-            if (walkPointSet)
-                agent.SetDestination(walkPoint);
-
-            Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-            //Walkpoint reached
-            if (distanceToWalkPoint.magnitude < 1f)
-                walkPointSet = false;
-        }
-        private void SearchWalkPoint()
-        {
-            //Calculate random point in range
-            float randomZ = Random.Range(-walkPointRange, walkPointRange);
-            float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-            walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-            if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-                walkPointSet = true;
-        }
-/*
-        private void ChasePlayer()
-        {
-            agent.SetDestination(player.position);
-        }
-
-         private void AttackPlayer()
-        {
-
-        }
-
-        public void TakeDamage(int damage)
-        {
-            health -= damage;
-
-            if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
-        }
-        private void DestroyEnemy()
-        {
-            Destroy(gameObject);
-        }
-
-        private void OnDrawGizmosSelected()
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, attackRange);
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(transform.position, sightRange);
-        }
-    */
+    private void RotateToTarget(Transform target)
+    {
+        Vector3 direction=(target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0,direction.z));
+        transform.rotation=Quaternion.Slerp(transform.rotation,lookRotation, Time.deltaTime *3f);
+    }
 }
